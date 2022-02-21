82bd826b51393aeb4930e0179a83fd978bbb8ed1
Amit Kumar <61462914+ak877157@users.noreply.github.com>
2022-02-21 11:14:03 +0530

    Update UserMainCode.java

--- Core_Java_Surprise_Test/src/com/org/gen/test/question1/UserMainCode.java
+++ Core_Java_Surprise_Test/src/com/org/gen/test/question1/UserMainCode.java
@@ -1,7 +1,17 @@
 package com.org.gen.test.question1;
 import java.util.Scanner;
 
-public class UserMainCode {
+class UserMainCode{
+	public static int calculateElectricBill(String inp1, String inp2, int inp3) {
+	    int n1 = Integer.parseInt(inp1.substring(5, inp1.length()));
+	    int n2 = Integer.parseInt(inp2.substring(5, inp2.length()));
+	    int n = Math.abs((n2 - n1) * inp3);
+	    return n;
+	  }
+	
+}
+
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,17 +24,12 @@ public class UserMainCode {
 	    String s2 = sc.nextLine();
 	    System.out.println(" Enter per unit charge = ");
 	    int n1 = sc.nextInt();
-	    
-	    int n2 = calculateElectricBill(s1, s2, n1);
+	   
+	    UserMainCode u1 = new UserMainCode();
+	    int n2 = u1.calculateElectricBill(s1, s2, n1);
 	    System.out.println(" Electric Bill is = " +n2);
 		
 	}
 	
-	public static int calculateElectricBill(String inp1, String inp2, int inp3) {
-	    int n1 = Integer.parseInt(inp1.substring(5, inp1.length()));
-	    int n2 = Integer.parseInt(inp2.substring(5, inp2.length()));
-	    int n = Math.abs((n2 - n1) * inp3);
-	    return n;
-	  }
 
 }
