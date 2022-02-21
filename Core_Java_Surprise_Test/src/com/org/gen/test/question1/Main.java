c3349d9d8f9a38798deb51fdb82d549d54d616c3
ak877157 <Amit.Kumar12@genpact.com>
2022-02-21 11:32:25 +0530

    Files Update

---
+++ Core_Java_Surprise_Test/src/com/org/gen/test/question1/Main.java
@@ -0,0 +1,35 @@
+package com.org.gen.test.question1;
+import java.util.Scanner;
+
+class UserMainCode{
+	public static int calculateElectricBill(String inp1, String inp2, int inp3) {
+	    int n1 = Integer.parseInt(inp1.substring(5, inp1.length()));
+	    int n2 = Integer.parseInt(inp2.substring(5, inp2.length()));
+	    int n = Math.abs((n2 - n1) * inp3);
+	    return n;
+	  }
+	
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your last reading = ");
+	    String s1 = sc.nextLine();
+	    System.out.println("Enter your current reading = ");
+	    String s2 = sc.nextLine();
+	    System.out.println(" Enter per unit charge = ");
+	    int n1 = sc.nextInt();
+	   
+	    UserMainCode u1 = new UserMainCode();
+	    int n2 = u1.calculateElectricBill(s1, s2, n1);
+	    System.out.println(" Electric Bill is = " +n2);
+		
+	}
+	
+
+}
